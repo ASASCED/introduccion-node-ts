63bfa40561410bd5f7725da128756aaf81d09fb2
Anix <jesus_heaven117@outlook.com>
2020-01-31 11:24:28 -0600

    Last changes

---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+export function hola(): void {
+  console.log("Hola Mundo!!!");
+}
+
+export function holaPersonalizado(nombre: string): void {
+  console.log(`Hola ${nombre}`);
+}
+
+export function holaAlCursoNPM(): void {
+  console.log("Hola al curso NPM!!!");
+}
